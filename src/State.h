cf1539d8e41458e80b15d4c1d56b8a56c26f6987
Sanjay Narayanan <36492151+brtsanjay@users.noreply.github.com>
2019-04-11 15:10:24 -0700

    Add validity check for state in constructor. (#4)

--- src/State.h
+++ src/State.h
@@ -3,7 +3,16 @@
 
 #include "path_finding/Cost.h"
 
+#include <stdexcept>
+
 namespace path_finding {
+
+// This function checks the validity of a provided state. By default, the function always returns valid.
+// Specialize this for custom validity checking for a given state type.
+template <typename StateType>
+bool isValid(StateType const& state) {
+	return true;
+}
 
 // This class represents a generic state of any type, and its associated cost.
 // The cost is a representation of the value obtained from a suitable heuristic function
@@ -11,7 +20,12 @@ namespace path_finding {
 template <typename StateType>
 class State {
 public:
-	State(StateType const& state) : _state{state}, _cost{computeCost(_state)} {}
+	State(StateType const& state) : _state{state}, _cost{computeCost(_state)} {
+		if (!isValid(_state)) {
+			throw ::std::runtime_error("Provided state is invalid.");
+		}
+	}
+
 	StateType getState() const { return _state; }
 	double getCost () const { return _cost; }
 
