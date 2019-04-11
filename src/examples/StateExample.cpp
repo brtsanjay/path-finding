5a72720c14b2f7a9dd921c900dd002de9965edb0
Sanjay Narayanan <sanjay.narayanan@bluerivert.com>
2019-04-11 14:57:58 -0700

    Add validity check for state in constructor.

--- src/examples/StateExample.cpp
+++ src/examples/StateExample.cpp
@@ -1,12 +1,22 @@
 #include "path_finding/Cost.h"
 #include "path_finding/State.h"
 
+#include <exception>
 #include <iostream>
 
 // Specialization to compute cost of int states.
 template <>
 double path_finding::computeCost<int>(int const& state) {
 	return state;
+}
+
+// Specialization to check validity of int states.
+template <>
+bool path_finding::isValid<int>(int const& state) {
+	if (state < 0) {
+		return false;
+	}
+	return true;
 }
 
 int main() {
@@ -17,6 +27,16 @@ int main() {
 		::std::cout << "State<int>\n";
 		::std::cout << "State: " << state.getState() << '\n';
 		::std::cout << "Cost: " << state.getCost() << '\n';
+	}
+	::std::cout << '\n';
+	{
+		// invalid int state
+		int constexpr intState = -1;
+		try {
+			path_finding::State state{intState};
+		} catch (::std::exception const& e) {
+			::std::cerr << "Exception: " << e.what() << '\n';
+		}
 	}
 	::std::cout << '\n';
 	{
